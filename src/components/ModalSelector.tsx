45ecae43ea643f04a56fe8873a0a092a0ab89212
djohnson <mr.demarcus.johnson@gmail.com>
2024-02-23 03:40:45 +0900

    Add a link to get more models in model selector

--- src/components/ModalSelector.tsx
+++ src/components/ModalSelector.tsx
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react-lite'
 
 import ChevronDown from '../icons/ChevronDown'
+import Globe from '../icons/Globe'
+
 import { settingStore } from '../models/SettingStore'
 
 const ModalSelector = observer(() => {
@@ -15,13 +17,13 @@ const ModalSelector = observer(() => {
 
       <ul
         tabIndex={0}
-        className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52 mt-2 border border-base-content/30"
+        className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box mt-2 border border-base-content/30 max-h-52 flex flex-col"
       >
         {models?.map(model => (
           <li key={model.name}>
             <input
               type="radio"
-              className="btn btn-sm btn-block btn-ghost justify-start"
+              className="btn btn-sm btn-ghost w-full place-items-center"
               aria-label={model.name}
               value={model.name}
               checked={selectedModel === model}
@@ -29,6 +31,13 @@ const ModalSelector = observer(() => {
             />
           </li>
         ))}
+
+        <li>
+          <a href="https://ollama.com/library" className="btn btn-outline btn-sm btn-neutral place-items-center flex flex-row flex-nowrap gap-2 w-full mt-2">
+            <span className=" text-sm whitespace-nowrap ">Ollama Library</span>
+            <Globe />
+          </a>
+        </li>
       </ul>
     </div>
   )
