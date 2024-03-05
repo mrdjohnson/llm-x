44a9c83da8efb0beb6c541dae39f6de076ebf032
djohnson <mr.demarcus.johnson@gmail.com>
2024-03-04 22:57:34 -0800

    Fix: Prettier should run on main staged files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
