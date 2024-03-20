d02f8a029038c52a80b78d70804770cda62111de
djohnson <mr.demarcus.johnson@gmail.com>
2024-03-19 19:47:23 -0700

    Increase default keep alive time

--- src/utils/OllamaApi.ts
+++ src/utils/OllamaApi.ts
@@ -64,6 +64,7 @@ export class OllmaApi {
     const chatOllama = new ChatOllama({
       baseUrl: host,
       model,
+      keepAlive: '20m',
     }).bind({ signal: OllmaApi.abortController.signal })
 
     const stream = await ChatPromptTemplate.fromMessages(messages)
