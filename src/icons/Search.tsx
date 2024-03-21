ce8871dd1284a9356c084b6615fcd9d75d2e51b0
djohnson <mr.demarcus.johnson@gmail.com>
2024-03-22 07:33:24 +0900

    Search Icon should be cooler

--- src/icons/Search.tsx
+++ src/icons/Search.tsx
@@ -1,17 +1,12 @@
+// https://icon-sets.iconify.design/fluent/calendar-search-16-regular/
+// the calendar looked liked the omnibar to me, hopefully not to be confused with a calendar
+
 export default function Search() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className="h-4 w-4"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" className="h-4 w-5">
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+        fill="currentColor"
+        d="M11.5 2A2.5 2.5 0 0 1 14 4.5v7a2.5 2.5 0 0 1-2.5 2.5H9.121l-1-1H11.5a1.5 1.5 0 0 0 1.5-1.5V6H3v.027a4.474 4.474 0 0 0-1 .23V4.5A2.5 2.5 0 0 1 4.5 2zm0 1h-7A1.5 1.5 0 0 0 3 4.5V5h10v-.5A1.5 1.5 0 0 0 11.5 3m-8 11c.786 0 1.512-.26 2.096-.697l2.55 2.55a.5.5 0 1 0 .708-.707l-2.55-2.55A3.5 3.5 0 1 0 3.5 14m0-1a2.5 2.5 0 1 1 0-5a2.5 2.5 0 0 1 0 5"
       />
     </svg>
   )
