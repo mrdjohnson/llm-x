a0288e2da5c50e921fa43c0557cfa84a59525673
djohnson <mr.demarcus.johnson@gmail.com>
2024-03-25 20:18:28 +0900

    Add default rules for eslint, add eslint to precommit hook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,17 +6,51 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
+
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+    'import/no-unresolved': 'error',
+    'import/no-relative-packages': 'error',
+    'import/no-self-import': 'error',
+    'import/newline-after-import': ['error', { count: 1, exactCount: true }],
+    'import/no-duplicates': 'error',
+
+    'no-restricted-imports': 'off',
+    '@typescript-eslint/no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['../*'],
+            message: 'No relative imports, please start with ~ instead',
+          },
+          {
+            group: ['./*'],
+            message: 'No relative imports, please start with ~ instead',
+          },
+        ],
+      },
+    ],
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
   },
 }
