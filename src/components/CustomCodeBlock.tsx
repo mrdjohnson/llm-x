36d64a45ccd919f50ae87bbfa46dc2e968b7ec20
djohnson <mr.demarcus.johnson@gmail.com>
2024-03-25 10:17:04 +0900

    Display the language for highlighted code blocks

--- src/components/CustomCodeBlock.tsx
+++ src/components/CustomCodeBlock.tsx
@@ -7,7 +7,7 @@ import hljs from 'highlight.js/lib/common'
 import 'highlight.js/styles/base16/woodland.min.css'
 
 const CustomCodeBlock = (props: React.HTMLAttributes<HTMLElement>) => {
-  const { children, className = '', ...rest } = props
+  const { children, ...rest } = props
 
   const text = children?.toString() || ''
 
@@ -15,19 +15,26 @@ const CustomCodeBlock = (props: React.HTMLAttributes<HTMLElement>) => {
 
   const copy = () => navigator.clipboard.writeText(text)
 
-  const highlightedText = useMemo(() => {
-    return hljs.highlightAuto(text).value
+  const { highlightedText, language } = useMemo(() => {
+    const hightligted = hljs.highlightAuto(text)
+
+    return {
+      highlightedText: hightligted.value,
+      language: hightligted.language,
+    }
   }, [text])
 
   if (multiLine) {
     return (
-      <div className="indicator inline-grid w-full pr-2 ">
+      <div className="indicator inline-grid w-full pr-2 pt-4">
         <button
           className="indicator-item z-10 text-neutral-content/30 hover:text-neutral-content "
           onClick={copy}
         >
           <Copy />
         </button>
+
+        <div className="absolute -left-2 -top-2 text-sm opacity-30">{language}</div>
 
         <code
           {...rest}
