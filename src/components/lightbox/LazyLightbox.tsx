3f41f950d9a9ca93cb4d2b9ddfbcb013e49aa06c
djohnson <mr.demarcus.johnson@gmail.com>
2024-04-06 22:00:03 +0900

    fix: Lightbox images should be downloadable from slide

--- src/components/lightbox/LazyLightbox.tsx
+++ src/components/lightbox/LazyLightbox.tsx
@@ -4,9 +4,11 @@ import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails'
 import Download from 'yet-another-react-lightbox/plugins/download'
 import Captions from 'yet-another-react-lightbox/plugins/captions'
 import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import _ from 'lodash'
 
 import { chatStore } from '~/models/ChatStore'
 import CachedImage from '~/components/CachedImage'
+import CachedStorage from '~/utils/CachedStorage'
 
 import 'yet-another-react-lightbox/styles.css'
 import 'yet-another-react-lightbox/plugins/thumbnails.css'
@@ -26,6 +28,21 @@ const LazyLightbox = observer(() => {
     <Lightbox
       close={chat.closeLightbox}
       plugins={[Thumbnails, Download, Captions, Zoom]}
+      download={{
+        download: ({ slide, saveAs }) => {
+          CachedStorage.get(slide.src).then(imageData => {
+            if (!imageData) return
+
+            let name: string | undefined
+
+            if (_.isString(slide.description)) {
+              name = _.snakeCase(slide.description)
+            }
+
+            return saveAs(imageData, name)
+          })
+        },
+      }}
       index={index}
       carousel={{ finite: true }}
       on={{
