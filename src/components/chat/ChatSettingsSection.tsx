fd8d90f33bf806ab93df9f26212585b9901735ac
djohnson <mr.demarcus.johnson@gmail.com>
2024-04-09 13:31:29 +0900

    Individual Chat export should include image option

--- src/components/chat/ChatSettingsSection.tsx
+++ src/components/chat/ChatSettingsSection.tsx
@@ -1,5 +1,11 @@
-import { useEffect } from 'react'
-import { AccordionItem, AccordionButton, AccordionIcon, AccordionPanel } from '@chakra-ui/react'
+import { useEffect, useRef } from 'react'
+import {
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  useDisclosure,
+} from '@chakra-ui/react'
 import _ from 'lodash'
 import { observer } from 'mobx-react-lite'
 import { useForm } from 'react-hook-form'
@@ -24,6 +30,13 @@ export const ChatSettingsSection = observer(
       formState: { isDirty },
     } = useForm<{ name: string }>({})
 
+    const {
+      isOpen: isExportOpen,
+      onOpen: openExportDialog,
+      onClose: onExportClose,
+    } = useDisclosure()
+    const modalRef = useRef<HTMLDialogElement>(null)
+
     const selectedChat = chatStore.selectedChat
     const chat = chatStore.selectedChat!
 
@@ -35,8 +48,12 @@ export const ChatSettingsSection = observer(
       reset()
     })
 
-    const exportChat = async () => {
-      const data = JSON.stringify(await ChatSnapshotHandler.formatChatToExport(chat))
+    const exportChat = async (includeImages: boolean) => {
+      const data = JSON.stringify(
+        await ChatSnapshotHandler.formatChatToExport(chat, { includeImages }),
+      )
+
+      onExportClose()
 
       const link = document.createElement('a')
       link.href = URL.createObjectURL(new Blob([data], { type: 'application/json' }))
@@ -50,80 +67,131 @@ export const ChatSettingsSection = observer(
       reset()
     }, [chat.name])
 
+    useEffect(() => {
+      if (isExportOpen) {
+        modalRef.current?.showModal()
+      } else {
+        modalRef.current?.close()
+      }
+    }, [isExportOpen])
+
     if (!selectedChat) return null
 
     return (
-      <AccordionItem
-        className={
-          isOpen
-            ? 'flex min-h-[48px] flex-1 flex-col overflow-y-hidden [&>.chakra-collapse]:!flex [&>.chakra-collapse]:!flex-1'
-            : ''
-        }
-      >
-        <AccordionButton className="w-full self-start" onClick={onSectionClicked}>
-          <button
-            className={'btn max-w-full flex-1 flex-nowrap px-2' + (isOpen ? ' btn-neutral' : '')}
-          >
-            <span className="flex-shrink-1 line-clamp-1 max-w-[85%]">
-              {selectedChat?.name || 'new chat'}
-            </span>
-            <AccordionIcon className="-flex-1" />
-          </button>
-        </AccordionButton>
+      <>
+        <AccordionItem
+          className={
+            isOpen
+              ? 'flex min-h-[48px] flex-1 flex-col overflow-y-hidden [&>.chakra-collapse]:!flex [&>.chakra-collapse]:!flex-1'
+              : ''
+          }
+        >
+          <AccordionButton className="w-full self-start" onClick={onSectionClicked}>
+            <button
+              className={'btn max-w-full flex-1 flex-nowrap px-2' + (isOpen ? ' btn-neutral' : '')}
+            >
+              <span className="flex-shrink-1 line-clamp-1 max-w-[85%]">
+                {selectedChat?.name || 'new chat'}
+              </span>
+              <AccordionIcon className="-flex-1" />
+            </button>
+          </AccordionButton>
 
-        <AccordionPanel flex={1} className=" mt-2 flex flex-1 flex-col text-base-content">
-          <div className="no-scrollbar flex h-full flex-1 flex-col overflow-y-scroll rounded-md">
-            <div className="flex flex-col gap-2 rounded-box bg-base-300 text-base-content">
-              <form className="flex w-full flex-row gap-2" onSubmit={handleFormSubmit}>
-                <div className="form-control w-full">
-                  <div className="label pb-1 pt-0">
-                    <span className="label-text text-sm">Name:</span>
+          <AccordionPanel flex={1} className=" mt-2 flex flex-1 flex-col text-base-content">
+            <div className="no-scrollbar flex h-full flex-1 flex-col overflow-y-scroll rounded-md">
+              <div className="flex flex-col gap-2 rounded-box bg-base-300 text-base-content">
+                <form className="flex w-full flex-row gap-2" onSubmit={handleFormSubmit}>
+                  <div className="form-control w-full">
+                    <div className="label pb-1 pt-0">
+                      <span className="label-text text-sm">Name:</span>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-2">
+                      <input
+                        type="text"
+                        id="name"
+                        className="input input-bordered w-full flex-1 focus:outline-none"
+                        defaultValue={chat.name || 'new chat'}
+                        minLength={2}
+                        maxLength={30}
+                        {...register('name', { required: true, minLength: 2, maxLength: 30 })}
+                      />
+
+                      {isDirty && (
+                        <button className="btn btn-neutral">
+                          <Check />
+                        </button>
+                      )}
+                    </div>
                   </div>
+                </form>
+              </div>
+            </div>
 
-                  <div className="flex flex-row items-center gap-2">
-                    <input
-                      type="text"
-                      id="name"
-                      className="input input-bordered w-full flex-1 focus:outline-none"
-                      defaultValue={chat.name || 'new chat'}
-                      minLength={2}
-                      maxLength={30}
-                      {...register('name', { required: true, minLength: 2, maxLength: 30 })}
-                    />
+            <div className="flex flex-row gap-2">
+              <Tooltip label="Export Chat">
+                <button
+                  onClick={openExportDialog}
+                  className="btn btn-ghost flex flex-1"
+                  title="Export Chat"
+                >
+                  <DocumentArrowDown />
+                </button>
+              </Tooltip>
 
-                    {isDirty && (
-                      <button className="btn btn-neutral">
-                        <Check />
-                      </button>
-                    )}
-                  </div>
-                </div>
+              <Tooltip label="Delete Chat">
+                <button
+                  onClick={() => chatStore.deleteChat(chat)}
+                  className="btn btn-ghost text-error"
+                >
+                  <Delete className="h-5 w-5" />
+                </button>
+              </Tooltip>
+            </div>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <dialog className="modal modal-middle" ref={modalRef} onClose={onExportClose}>
+          <div className="modal-box">
+            <h3 className="mb-4 text-lg font-bold">Export type:</h3>
+
+            <div className="flex flex-row justify-between px-6">
+              <div className="flex flex-col">
+                <button className="btn btn-neutral" onClick={() => exportChat(true)}>
+                  With images
+                </button>
+
+                <span className="label-text">(larger file size)</span>
+              </div>
+
+              <div className="flex flex-col">
+                <button
+                  className="btn btn-neutral"
+                  onClick={() => {
+                    exportChat(false)
+                  }}
+                >
+                  Without images
+                </button>
+
+                <span className="label-text text-right">(smaller file size)</span>
+              </div>
+            </div>
+
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+                  ✕
+                </button>
               </form>
             </div>
           </div>
 
-          <div className="flex flex-row gap-2">
-            <Tooltip label="Export Chat">
-              <button
-                onClick={exportChat}
-                className="btn btn-ghost flex flex-1"
-                title="Export Chat"
-              >
-                <DocumentArrowDown />
-              </button>
-            </Tooltip>
-
-            <Tooltip label="Delete Chat">
-              <button
-                onClick={() => chatStore.deleteChat(chat)}
-                className="btn btn-ghost text-error"
-              >
-                <Delete className="h-5 w-5" />
-              </button>
-            </Tooltip>
-          </div>
-        </AccordionPanel>
-      </AccordionItem>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </>
     )
   },
 )
