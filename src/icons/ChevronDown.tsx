c1805caf7fe5b283bcffd8d330b827a119d27c9d
djohnson <mr.demarcus.johnson@gmail.com>
2024-04-15 14:58:36 +0900

    create custom number input form element

--- src/icons/ChevronDown.tsx
+++ src/icons/ChevronDown.tsx
@@ -1,12 +1,11 @@
-export default function ChevronDown() {
+export default function ChevronDown({ className = '' }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
       stroke="currentColor"
-      className="h-4 w-4"
+      className={'h-4 w-4 stroke-[1.5] ' + className}
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
     </svg>
