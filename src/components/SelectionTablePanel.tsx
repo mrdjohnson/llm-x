10f2b8d416b4fc26276808652c37df6aff67778c
djohnson <mr.demarcus.johnson@gmail.com>
2024-05-14 11:39:04 +0900

    Add SelectionTable header key for sort types
    force key type to be string or number

--- src/components/SelectionTablePanel.tsx
+++ src/components/SelectionTablePanel.tsx
@@ -134,7 +134,7 @@ const SelectionPanelTable = observer(
             <thead className="sticky top-0 z-20 bg-base-300 text-base-content">
               <tr>
                 {sortTypes.map(sortType => (
-                  <th>
+                  <th key={sortType.value as string | number}>
                     <span
                       className={
                         'tooltip tooltip-bottom flex w-fit select-none flex-row items-center' +
