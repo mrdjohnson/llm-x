6b79c5cd8fb1af9f3840fcff4a316e835c7f24a4
djohnson <mr.demarcus.johnson@gmail.com>
2024-06-19 12:34:43 +0700

    Nextui + daisyui error colors should match

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,8 @@
 import { nextui } from '@nextui-org/react'
 import themes from 'daisyui/src/theming/themes'
 
+const errorColor = 'oklch(51% 0.17 22.1)'
+
 module.exports = {
   content: [
     'src/**/*.{js,ts,jsx,tsx}',
@@ -12,17 +14,41 @@ module.exports = {
     extend: {},
   },
   darkMode: 'class',
-  plugins: [require('@tailwindcss/typography'), require('daisyui'), nextui()],
+  plugins: [require('@tailwindcss/typography'), require('daisyui'), nextui({
+    themes: {
+      light: {
+        colors: {
+          danger: errorColor
+        }
+      },
+      dark: {
+        colors: {
+          danger: errorColor
+        }
+      }
+    }
+  })],
   daisyui: {
     themes: [
       {
         garden: {
           ...themes['garden'],
           primary: 'oklch(62.45% 0.1947 3.83636)',
+          error: errorColor
         },
       },
-      'dark',
-      'dracula',
+      {
+        dark: {
+          ...themes['dark'],
+          error: errorColor
+        },
+      },
+      {
+        dracula: {
+          ...themes['dracula'],
+          error: errorColor
+        },
+      },
     ],
   },
 }
