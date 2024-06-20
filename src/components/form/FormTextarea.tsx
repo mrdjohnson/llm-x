80c6748bfcd08d01c5ecf2f0065f74876d26ff7a
djohnson <mr.demarcus.johnson@gmail.com>
2024-06-20 11:03:25 +0700

    Create a shared styled form text area component

---
+++ src/components/form/FormTextarea.tsx
@@ -0,0 +1,31 @@
+import { Textarea } from '@nextui-org/react'
+import { forwardRef } from 'react'
+
+// Weird typescript issue where it did not recognize the full typing of TextAreaProps
+type FormTextareaProps = Omit<Parameters<typeof Textarea>[0], 'isInvalid'>
+
+const FormTextarea = forwardRef((textareaProps: FormTextareaProps) => {
+  const isDisabled = textareaProps.disabled || textareaProps.isDisabled
+  const isInvalid = !isDisabled && !!textareaProps.errorMessage
+
+  return (
+    <div className={'w-full' + (isInvalid ? ' *:!text-error' : '')}>
+      <Textarea
+        type="text"
+        variant="bordered"
+        isInvalid={isInvalid}
+        isDisabled={isDisabled}
+        classNames={{
+          label: isInvalid ? '!text-error' : '!text-base-content/45',
+          inputWrapper:
+            '!bg-base-transparent border-base-content/30' +
+            (isInvalid ? ' !border-error' : '') +
+            (isDisabled ? ' opacity-30 hover:!border-base-content/30' : ''),
+        }}
+        {...textareaProps}
+      />
+    </div>
+  )
+})
+
+export default FormTextarea
