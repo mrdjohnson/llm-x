252196b58b9e473a1423178f1461a5ac30a2aed3
djohnson <mr.demarcus.johnson@gmail.com>
2024-07-13 08:37:49 +0200

    Refresh model button should look like a button

--- src/features/settings/panels/model/NotConnectedPanelSection.tsx
+++ src/features/settings/panels/model/NotConnectedPanelSection.tsx
@@ -6,14 +6,14 @@ import { ServerConnectionTypes } from '~/features/connections/servers'
 const NotConnectedPanelSection = ({ connection }: { connection: ServerConnectionTypes }) => {
   const openConnectionSettings = () => {
     connectionModelStore.dataStore.setSelectedConnectionById(connection.id)
-    settingStore.openSettingsModal('general')
+    settingStore.openSettingsModal('connections')
   }
 
   return (
     <div className="flex w-full flex-col justify-center gap-3">
-      <span className="flex justify-center gap-2 align-baseline text-lg font-semibold">
-        {connection.label} is not currently active
-        <button onClick={() => connection.fetchLmModels()}>
+      <span className="flex justify-center gap-2 text-lg font-semibold items-center">
+        Unable to get models for {connection.label}
+        <button className='btn btn-ghost btn-xs px-1' onClick={() => connection.fetchLmModels()}>
           <Refresh />
         </button>
       </span>
