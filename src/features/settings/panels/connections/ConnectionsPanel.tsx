ec5402b6e762bc665db5e78cfe09fbe312f2073c
djohnson <mr.demarcus.johnson@gmail.com>
2024-07-28 01:03:17 +0800

    ConnectionsPanel: apply settings template

--- src/features/settings/panels/connections/ConnectionsPanel.tsx
+++ src/features/settings/panels/connections/ConnectionsPanel.tsx
@@ -1,79 +1,53 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { observer } from 'mobx-react-lite'
-import { ScrollShadow, Tab, Tabs } from '@nextui-org/react'
-import _ from 'lodash'
+
+import SettingSection, { SettingSectionItem } from '~/containers/SettingSection'
 
 import NewConnectionPanel from '~/features/settings/panels/connections/NewConnectionPanel'
-
 import { connectionModelStore } from '~/features/connections/ConnectionModelStore'
 import ConnectionPanel from '~/features/settings/panels/connections/ConnectionPanel'
+import { ServerConnectionTypes } from '~/features/connections/servers'
 
 const ConnectionsPanel = observer(() => {
   const { selectedConnectionModelId, connections } = connectionModelStore
 
-  const [selectedTabId, setSelectedTabId] = useState<string>(selectedConnectionModelId ?? 'App')
-
-  useEffect(() => {
-    setSelectedTabId(selectedConnectionModelId ?? 'App')
+  const selectedConnection = useMemo(() => {
+    return connectionModelStore.getConnectionById(selectedConnectionModelId)
   }, [selectedConnectionModelId])
 
-  const selectedPanel = useMemo(() => {
-    if (selectedTabId === 'new_connection') {
-      return <NewConnectionPanel />
-    }
+  const connectionToSectionItem = (
+    connection: ServerConnectionTypes,
+  ): SettingSectionItem<ServerConnectionTypes> => ({
+    id: connection.id,
+    label: connection.label,
+    subLabels: [connection.enabled ? 'Enabled' : 'Disabled'],
+    data: connection,
+  })
 
-    const connection = connectionModelStore.getConnectionById(selectedTabId)
+  const items: Array<SettingSectionItem<ServerConnectionTypes>> = useMemo(() => {
+    return connections.map(connectionToSectionItem)
+  }, [connections])
 
-    if (connection) {
-      return <ConnectionPanel connection={connection} />
-    }
-
-    setSelectedTabId('new_connection')
-
-    return null
-  }, [selectedTabId, connections])
+  const itemFilter = (connection: ServerConnectionTypes, filterText: string) => {
+    return connection.label.toLowerCase().includes(filterText)
+  }
 
   return (
-    <div className="flex w-full flex-col">
-      <ScrollShadow orientation="horizontal" className="border-b border-base-content/30">
-        <Tabs
-          aria-label="Options"
-          variant="underlined"
-          selectedKey={selectedTabId}
-          onSelectionChange={key => setSelectedTabId(_.toString(key))}
-          classNames={{
-            tab: 'overflow-hidden flex-shrink-0 w-fit',
-            tabList:
-              'gap-3 w-full flex max-w-full relative rounded-none p-0 overflow-x-scroll flex-shrink-0',
-            cursor: 'group-data-[selected=true]:bg-primary w-full bg-base-content',
-            tabContent:
-              'group-data-[selected=true]:text-primary group-data-[selected=true]:border-b-primary flex-shrink-0 group-[.is-active-parent]:text-primary/60',
-          }}
-        >
-          {_.map(connections, connection => (
-            <Tab
-              key={connection.id}
-              title={connection.label}
-              className={connection.id === selectedConnectionModelId ? 'is-active-parent' : ''}
-            />
-          ))}
-
-          <Tab
-            key="new_connection"
-            title="New Connection +"
-            className={selectedConnectionModelId === 'new_connection' ? 'is-active-parent' : ''}
-          />
-        </Tabs>
-      </ScrollShadow>
-
-      {selectedPanel && (
-        <div className="flex-1 overflow-y-hidden pt-2">
-          <ScrollShadow className="flex h-full max-h-full w-full place-content-center">
-            {selectedPanel}
-          </ScrollShadow>
-        </div>
-      )}
-    </div>
+    <SettingSection
+      items={items}
+      filterProps={{
+        helpText: 'Filter connections by label...',
+        itemFilter,
+        emptyLabel: 'No connections found',
+      }}
+      addButtonProps={{
+        label: 'Add New Connection',
+      }}
+      emptySectionPanel={<NewConnectionPanel />}
+      renderItemSection={connection => <ConnectionPanel connection={connection} />}
+      selectedItem={selectedConnection && connectionToSectionItem(selectedConnection)}
+      allowSmallItems={false}
+    />
   )
 })
 
--- src/features/settings/panels/connections/NewConnectionPanel.tsx
+++ src/features/settings/panels/connections/NewConnectionPanel.tsx
@@ -5,17 +5,27 @@ import _ from 'lodash'
 import { connectionModelStore } from '~/features/connections/ConnectionModelStore'
 import { serverConnectionByType } from '~/features/connections/servers'
 
+import { settingStore } from '~/models/SettingStore'
+
 const NewConnectionPanel = observer(() => {
   return (
-    <div className="flex flex-col gap-4 mt-4 *:text-center">
-      <span>Select a connection type: </span>
+    <div className="mx-auto mt-4 flex flex-col gap-4 *:text-center">
+      <span>Select a connection type to add: </span>
 
       <Listbox>
-        {_.map(serverConnectionByType, connector => connector.getSnapshot()).map(({ type, label }) => (
-          <ListboxItem key={type} onClick={() => connectionModelStore.addConnection(type)}>
-            {label}
-          </ListboxItem>
-        ))}
+        {_.map(serverConnectionByType, connector => connector.getSnapshot()).map(
+          ({ type, label }) => (
+            <ListboxItem
+              key={type}
+              onClick={() => {
+                connectionModelStore.addConnection(type)
+                settingStore.openSettingsModal('connections')
+              }}
+            >
+              {label}
+            </ListboxItem>
+          ),
+        )}
       </Listbox>
     </div>
   )
