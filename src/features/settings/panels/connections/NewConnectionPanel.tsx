25f2b09aaae095068a08534847a63cde2c51c11a
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-06 16:42:35 +0900

    NewConnectionPanel: button colors should be theme based

--- src/features/settings/panels/connections/NewConnectionPanel.tsx
+++ src/features/settings/panels/connections/NewConnectionPanel.tsx
@@ -1,5 +1,4 @@
 import { observer } from 'mobx-react-lite'
-import { Listbox, ListboxItem } from '@nextui-org/listbox'
 import _ from 'lodash'
 
 import { connectionViewModelByType } from '~/core/connection/viewModels'
@@ -7,18 +6,20 @@ import { connectionStore } from '~/core/connection/ConnectionStore'
 
 const NewConnectionPanel = observer(() => {
   return (
-    <div className="mt-4 flex flex-col gap-4 *:text-center">
+    <div className="mx-auto mt-4 flex flex-col gap-4 *:text-center">
       <span>Select a connection type: </span>
 
-      <Listbox>
-        {_.map(connectionViewModelByType, getConnector => getConnector().getSnapshot()).map(
-          ({ type, label }) => (
-            <ListboxItem key={type} onClick={() => connectionStore.addConnection(type)}>
-              {label}
-            </ListboxItem>
-          ),
-        )}
-      </Listbox>
+      {_.map(connectionViewModelByType, getConnector => getConnector().getSnapshot()).map(
+        ({ type, label }) => (
+          <button
+            key={type}
+            onClick={() => connectionStore.addConnection(type)}
+            className="btn btn-outline btn-secondary w-fit place-self-center border-0"
+          >
+            {label}
+          </button>
+        ),
+      )}
     </div>
   )
 })
