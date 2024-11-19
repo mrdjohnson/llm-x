d1803a7c8ba045169f37457ee0fa2bdd6bd691ac
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-19 15:38:12 +0900

    refactor: move help panel to help folder

--- src/features/settings/settingRoutes.tsx
+++ src/features/settings/settingRoutes.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 import GeneralPanel from '~/features/settings/panels/general/GeneralPanel'
 import MobileSplashPanel from '~/features/settings/panels/MobileSplashPanel'
-import HelpPanel from '~/features/settings/panels/HelpPanel'
+import HelpPanel from '~/features/settings/panels/help/HelpPanel'
 import PersonaPanel from '~/features/settings/panels/PersonaPanel'
 
 import ModelPanel, { ConnectionModelPanel } from '~/features/settings/panels/model/ModelPanel'
