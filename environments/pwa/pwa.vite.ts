ba457b88eec826e956dd89527bfe320cdd891edd
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-20 22:20:40 +0900

    Logos: add maskable.app icons to fix logo display issues on mobile devices

--- environments/pwa/pwa.vite.ts
+++ environments/pwa/pwa.vite.ts
@@ -36,6 +36,42 @@ export const pwaOptions: Partial<VitePWAOptions> = {
         purpose: 'any',
       },
       {
+        sizes: '48x48',
+        src: 'maskable_icon_x48.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        sizes: '72x72',
+        src: 'maskable_icon_x72.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        sizes: '96x96',
+        src: 'maskable_icon_x96.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        sizes: '128x128',
+        src: 'maskable_icon_x128.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        sizes: '192x192',
+        src: 'maskable_icon_x192.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        sizes: '384x384',
+        src: 'maskable_icon_x384.png',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
         src: 'pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png',
