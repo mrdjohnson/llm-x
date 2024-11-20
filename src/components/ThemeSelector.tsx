1920d174f8b97d729f58bdf8ef0010a63d2c374e
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-20 10:19:38 +0900

    Theme selector should be based on next ui
    - closes on selection
    - closes on outside click
    - shows current theme selected
    - works of safari

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -1,10 +1,8 @@
 import _ from 'lodash'
 import { observer } from 'mobx-react-lite'
-import { useState } from 'react'
-
-import ChevronDown from '~/icons/ChevronDown'
 
 import { settingStore } from '~/core/setting/SettingStore'
+import { Select, SelectItem } from '@nextui-org/react'
 
 const themes = {
   _system: 'System theme',
@@ -14,52 +12,40 @@ const themes = {
 }
 
 const ThemeSelector = observer(() => {
-  const [isOpen, setIsOpen] = useState(false)
-
   const selectedTheme = settingStore.setting.theme
 
   return (
     <div className="form-control">
-      <div className="label pb-1 pt-0">
-        <span className="label-text text-sm">Theme:</span>
-      </div>
-
-      <div
-        className={'dropdown dropdown-bottom ' + (isOpen ? ' dropdown-open' : '')}
-        onFocus={() => setIsOpen(true)}
-        onBlur={() => setIsOpen(false)}
+      <Select
+        className="w-full min-w-[20ch] rounded-md border border-base-content/30 bg-transparent"
+        size="sm"
+        classNames={{
+          value: '!text-base-content min-w-[20ch]',
+          trigger: 'bg-base-100 hover:!bg-base-200 rounded-md',
+          popoverContent: 'text-base-content bg-base-100',
+        }}
+        selectedKeys={[selectedTheme]}
+        label="Theme"
+        onChange={selection =>
+          settingStore.update({ theme: selection.target.value as keyof typeof themes })
+        }
       >
-        <button role="button" className="btn btn-active w-full">
-          {themes[selectedTheme]}
-
-          <div
+        {_.map(themes, (value, key) => (
+          <SelectItem
+            key={key}
+            value={key}
             className={
-              'transition-transform duration-200 ease-in-out' + (isOpen ? ' rotate-180 ' : '')
+              'w-full !min-w-[13ch] text-base-content' +
+              (key === selectedTheme ? ' text-primary' : '')
             }
+            classNames={{
+              description: ' text',
+            }}
           >
-            <ChevronDown />
-          </div>
-        </button>
-
-        <ul className="dropdown-content z-[1] mt-1 w-full rounded-box bg-base-300 p-2 shadow-2xl">
-          <label className="text-sm text-base-content/60">Select theme</label>
-
-          {_.map(themes, (label: string, value: keyof typeof themes) => (
-            <li key={value}>
-              <button
-                className={
-                  'btn btn-ghost btn-sm btn-block justify-start' +
-                  (selectedTheme === value ? ' btn-active' : '')
-                }
-                aria-label={label}
-                onClick={() => settingStore.update({ theme: value })}
-              >
-                {label}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+            {value}
+          </SelectItem>
+        ))}
+      </Select>
     </div>
   )
 })
