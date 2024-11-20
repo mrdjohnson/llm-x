dab0086da3f6b858c61d6f70b5962d957c883c01
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-20 09:39:47 +0900

    Open model page or settings after user selects it

--- src/features/settings/panels/connections/NewConnectionPanel.tsx
+++ src/features/settings/panels/connections/NewConnectionPanel.tsx
@@ -1,10 +1,25 @@
 import { observer } from 'mobx-react-lite'
 import _ from 'lodash'
+import { useNavigate } from 'react-router-dom'
 
+import { ConnectionTypes } from '~/core/types'
 import { connectionViewModelByType } from '~/core/connection/viewModels'
 import { connectionStore } from '~/core/connection/ConnectionStore'
 
 const NewConnectionPanel = observer(() => {
+  const navigate = useNavigate()
+
+  const addConnection = async (type: ConnectionTypes) => {
+    const nextConnection = await connectionStore.addConnection(type)
+
+    navigate('/models')
+
+    // this lets the drawer close first....then opens it back up
+    setTimeout(() => {
+      navigate('/models/' + nextConnection.id)
+    }, 300)
+  }
+
   return (
     <div className="mx-auto mt-4 flex flex-col gap-4 *:text-center">
       <span>Select a connection type: </span>
@@ -13,7 +28,7 @@ const NewConnectionPanel = observer(() => {
         ({ type, label }) => (
           <button
             key={type}
-            onClick={() => connectionStore.addConnection(type)}
+            onClick={() => addConnection(type)}
             className="btn btn-outline btn-secondary w-fit place-self-center border-0"
           >
             {label}
