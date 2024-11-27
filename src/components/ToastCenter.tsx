aea677e0d920d168a081636c69abe57ee473bbfa
djohnson <mr.demarcus.johnson@gmail.com>
2024-11-27 14:18:11 +0900

    toasts should be centered on main content

--- src/components/ToastCenter.tsx
+++ src/components/ToastCenter.tsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react'
 import _ from 'lodash'
 import { observer } from 'mobx-react-lite'
 import { twMerge } from 'tailwind-merge'
+import useMedia from 'use-media'
 
 import { toastStore } from '~/core/ToastStore'
+import { settingStore } from '~/core/setting/SettingStore'
 
 const ToastCenter = observer(() => {
   const [hovering, setHovering] = useState(false)
+  const isMobile = useMedia('(max-width: 768px)')
 
   const { toasts } = toastStore
-  
+
   // this effect will run every render, yes
   // but it will also auto update whenever there is a change to toasts
   useEffect(() => {
@@ -27,13 +30,14 @@ const ToastCenter = observer(() => {
       className={twMerge(
         'toast z-30 mb-24 w-full gap-3 bg-transparent',
         _.isEmpty(toasts) && 'hidden',
+        settingStore.setting.isSidebarOpen && !isMobile && 'pl-[270px]',
       )}
       onMouseEnter={() => setHovering(true)}
       onMouseLeave={() => setHovering(false)}
     >
       {toasts.map(toast => (
         <div
-          className={`alert alert-${toast.type} relative rounded-md border border-base-content/30 px-4 text-xl font-bold`}
+          className={`alert alert-${toast.type} relative mx-auto max-w-4xl rounded-md border border-base-content/30 px-4 text-xl font-bold`}
           key={toast.id}
         >
           <span className="cursor-default whitespace-pre-wrap break-words">{toast.message}</span>
