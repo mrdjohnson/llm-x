5897fd1d59a8512fee349217fd11c48ef8e9f727
djohnson <mr.demarcus.johnson@gmail.com>
2025-07-18 23:37:49 +0700

    tests: refactor ConnectionModelFactory to add options

--- src/core/connection/ConnectionModel.factory.ts
+++ src/core/connection/ConnectionModel.factory.ts
@@ -3,27 +3,66 @@ import _ from 'lodash'
 
 import { ConnectionModel } from '~/core/connection/ConnectionModel'
 import { connectionViewModelByType, ConnectionViewModelTypes } from '~/core/connection/viewModels'
+import { LanguageModelFactory } from '~/core/LanguageModel.factory'
+import { BaseModelTypes } from '~/core/connection/types'
 
-export const ConnectionModelFactory = Factory.define<
-  ConnectionModel,
-  null,
-  ConnectionViewModelTypes
->(({ sequence, onCreate, params }) => {
-  onCreate(connectionModel => {
-    const ConnectionViewModelClass = connectionViewModelByType[connectionModel.type]()
+import { setServerResponseForModels } from '~/tests/helpers/setServerResponseForModels'
+import { connectionStore } from '~/core/connection/ConnectionStore'
 
-    return ConnectionViewModelClass.toViewModel(connectionModel)
-  })
+export type ConnectionFactoryOptions = {
+  connectionParams?: Partial<ConnectionModel>
 
-  const ConnectionViewModelClass = params.type
-    ? connectionViewModelByType[params.type]()
-    : _.sample(connectionViewModelByType)!()
+  modelCount?: number
+  models?: BaseModelTypes[]
+  modelParams?: Partial<BaseModelTypes>
+}
 
-  const snapshot = ConnectionViewModelClass.getSnapshot()
-
-  return {
-    ...snapshot,
-    ...params,
-    id: sequence.toString(),
+export const addModelsToConnection = async (
+  connection: ConnectionViewModelTypes,
+  { models, modelCount, modelParams }: ConnectionFactoryOptions,
+) => {
+  if (_.isNumber(modelCount) || !models) {
+    models = LanguageModelFactory.withOptions({
+      modelType: connection.type,
+      modelParams,
+    }).buildList(modelCount ?? 1)
   }
-})
+
+  setServerResponseForModels(connection, models)
+  await connection.fetchLmModels()
+}
+
+class ConnectionModelFactoryClass extends Factory<ConnectionModel, null, ConnectionViewModelTypes> {
+  withOptions(options: ConnectionFactoryOptions = {}) {
+    return this.params(options.connectionParams || {}).afterCreate(async connection => {
+      await addModelsToConnection(connection, options)
+
+      return connection
+    })
+  }
+}
+
+export const ConnectionModelFactory = ConnectionModelFactoryClass.define(
+  ({ sequence, onCreate, params }) => {
+    onCreate(async connectionModel => {
+      const connectionViewModel = await connectionStore.addConnection(
+        connectionModel.type,
+        connectionModel,
+      )
+
+      return connectionViewModel
+    })
+
+    const ConnectionViewModelClass = params.type
+      ? connectionViewModelByType[params.type]()
+      : _.sample(connectionViewModelByType)!()
+
+    const snapshot = ConnectionViewModelClass.getSnapshot()
+
+    return {
+      ...snapshot,
+      ...params,
+      id: sequence.toString(),
+    }
+  },
+)
