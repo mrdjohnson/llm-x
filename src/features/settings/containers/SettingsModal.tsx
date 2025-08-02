3eb1fcc2218a17422fdb563f86cab19054fabad3
djohnson <mr.demarcus.johnson@gmail.com>
2025-08-03 02:16:27 +0700

    refactor: migrate settings modal to mantine

--- src/features/settings/containers/SettingsModal.tsx
+++ src/features/settings/containers/SettingsModal.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useMemo, useRef } from 'react'
 import _ from 'lodash'
 import useMedia from 'use-media'
-import { Modal, ModalContent, ModalBody, Select, SelectItem } from '@heroui/react'
-import { NavLink, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
-import { twMerge } from 'tailwind-merge'
+import { NavLink as RouterLink, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Modal, NavLink, Select } from '@mantine/core'
 
 import DaisyUiThemeProvider from '~/containers/DaisyUiThemeProvider'
 import Drawer from '~/containers/Drawer'
@@ -33,17 +32,18 @@ const SettingsSidePanel = ({
       if (hidden) return null
 
       return (
-        <NavLink
-          to={panelName}
-          key={panelName}
-          className={({ isActive }) =>
-            twMerge('btn w-full justify-start', isActive && 'btn-neutral')
-          }
-          onClick={() => onSectionClick?.(panelName)}
-          role="link"
-        >
-          {label}
-        </NavLink>
+        <RouterLink to={panelName}>
+          {({ isActive }) => (
+            <NavLink
+              key={panelName}
+              className="rounded-md"
+              onClick={() => onSectionClick?.(panelName)}
+              role="link"
+              active={isActive}
+              label={label}
+            />
+          )}
+        </RouterLink>
       )
     },
   )
@@ -61,40 +61,25 @@ const MobileSettingsSidePanel = () => {
   }
 
   const routes = useMemo(() => {
-    return _.omitBy(settingRoutesByName, { hidden: true })
+    return _.chain(settingRoutesByName)
+      .omitBy({ hidden: true })
+      .map((value, key) => ({
+        value: '/' + key,
+        label: value!.label,
+      }))
+      .value()
   }, [])
 
   if (selectedPanel === '/search') return null
 
   return (
     <Select
-      className="w-full min-w-[20ch] rounded-md border border-base-content/30 bg-transparent"
-      size="sm"
-      classNames={{
-        value: '!text-base-content min-w-[20ch]',
-        trigger: 'bg-base-100 hover:!bg-base-200 rounded-md',
-        popoverContent: 'text-base-content bg-base-100',
-      }}
-      selectedKeys={[selectedPanel]}
+      value={selectedPanel}
+      size="md"
       label="Section"
-      onChange={selection => goToSection(selection.target.value)}
-    >
-      {_.map(routes, ({ label }: SettingPanelType, panelName) => (
-        <SelectItem
-          key={'/' + panelName}
-          value={'/' + panelName}
-          className={twMerge(
-            'w-full !min-w-[13ch] text-base-content',
-            panelName === selectedPanel && 'text-primary',
-          )}
-          classNames={{
-            description: ' text',
-          }}
-        >
-          {label}
-        </SelectItem>
-      ))}
-    </Select>
+      onChange={(_value, option) => goToSection(option.value)}
+      data={routes}
+    />
   )
 }
 
@@ -136,89 +121,90 @@ const SettingsModal = () => {
   //   [pathname],
   // )
 
+  const title = (
+    <DaisyUiThemeProvider>
+      <div className="navbar flex h-auto min-h-0 w-full max-w-full gap-1 p-0 text-base-content">
+        {shouldShowBackButton && (
+          <div className="ml-auto justify-end md:hidden">
+            <button
+              className="btn btn-circle btn-ghost btn-sm !text-lg opacity-70"
+              onClick={() => navigate(-1)}
+            >
+              <Back />
+            </button>
+          </div>
+        )}
+
+        <SettingSearchBar />
+
+        {!isSearching && (
+          <div className="ml-auto justify-end">
+            <NavButton to="/" className="btn btn-circle btn-ghost btn-sm opacity-70 md:text-lg">
+              ✕
+            </NavButton>
+          </div>
+        )}
+      </div>
+    </DaisyUiThemeProvider>
+  )
+
   return (
     <Modal
-      backdrop="opaque"
-      isOpen={isOpen}
+      opened={isOpen}
       onClose={() => navigate('/')}
-      size={isMobile ? 'full' : undefined}
+      size={'100%'}
+      className="w-full max-w-[1200px]"
       classNames={{
-        base: twMerge(!isMobile && '!w-full max-w-[1200px]'),
-        body: 'px-2 text-base-content overflow-hidden',
-        backdrop:
+        body: 'px-2 text-base-content overflow-hidden rounded-md',
+        overlay:
           'bg-gradient-to-t from-base-200 from-25% to-base-200/20 backdrop-opacity-20 !cursor-pointer',
-        closeButton: 'hover:bg-base-content/30 hover:text-base-content',
+        close: 'hover:bg-base-content/30 hover:text-base-content',
+        title: 'w-full',
       }}
-      hideCloseButton
+      fullScreen={isMobile}
+      withCloseButton={false}
+      title={title}
+      centered
     >
       <DaisyUiThemeProvider>
-        <ModalContent className="bg-base-100">
-          <div className="navbar flex h-auto min-h-0 max-w-full gap-1 text-base-content">
-            {shouldShowBackButton && (
-              <div className="ml-auto justify-end md:hidden">
-                <button
-                  className="btn btn-circle btn-ghost btn-sm !text-lg opacity-70"
-                  onClick={() => navigate(-1)}
-                >
-                  <Back />
-                </button>
-              </div>
-            )}
+        <div className="flex h-full flex-grow-0 flex-col justify-stretch gap-2 overflow-y-scroll md:h-[700px] md:flex-row">
+          <div
+            className="w-full rounded-lg bg-black/10 p-2 md:w-[200px] md:min-w-[200px] md:max-w-[200px]"
+            role="complementary"
+          >
+            <div className="flex w-full flex-col gap-2 rounded-md md:h-full">
+              {isMobile ? <MobileSettingsSidePanel /> : <SettingsSidePanel />}
 
-            <SettingSearchBar />
+              <a
+                href="https://github.com/mrdjohnson/llm-x"
+                className="btn btn-outline btn-neutral mt-auto hidden fill-base-content stroke-base-content hover:fill-primary-content md:flex"
+                aria-label="LLM-X's Github"
+                target="__blank"
+              >
+                <Github />
+              </a>
+            </div>
 
-            {!isSearching && (
-              <div className="ml-auto justify-end">
-                <NavButton to="/" className="btn btn-circle btn-ghost btn-sm opacity-70 md:text-lg">
-                  ✕
-                </NavButton>
-              </div>
-            )}
+            <div className="divider mb-0 mt-2 md:hidden" />
           </div>
 
-          <ModalBody className="p-0">
-            <div className="flex h-full flex-col rounded-md bg-base-100 p-2 pt-0">
-              <div className="flex h-full flex-grow-0 flex-col justify-stretch gap-2 overflow-y-scroll md:h-[700px] md:flex-row">
-                <div
-                  className="w-full md:w-[200px] md:min-w-[200px] md:max-w-[200px]"
-                  role="complementary"
-                >
-                  <div className="flex w-full flex-col gap-2 rounded-md bg-base-200 md:h-full md:p-2">
-                    {isMobile ? <MobileSettingsSidePanel /> : <SettingsSidePanel />}
-
-                    <a
-                      href="https://github.com/mrdjohnson/llm-x"
-                      className="btn btn-outline btn-neutral mt-auto hidden fill-base-content stroke-base-content hover:fill-primary-content md:flex"
-                      aria-label="LLM-X's Github"
-                      target="__blank"
-                    >
-                      <Github />
-                    </a>
-                  </div>
-
-                  <div className="divider mb-0 mt-2 md:hidden" />
-                </div>
-
-                <section className="flex h-full w-full flex-1 justify-stretch overflow-x-auto overflow-y-scroll">
-                  <Routes>
-                    {_.map(settingRoutesByName, ({ Component, label, ...rest }, key) => (
-                      <Route
-                        key={key}
-                        path={key + '/*'}
-                        element={
-                          <Drawer label={label} path={'/' + key}>
-                            <Component />
-                          </Drawer>
-                        }
-                        {...rest}
-                      />
-                    ))}
-                  </Routes>
-                </section>
-              </div>
-            </div>
-          </ModalBody>
-        </ModalContent>
+          <section className="flex h-full w-full flex-1 justify-stretch overflow-x-auto overflow-y-scroll">
+            <Routes>
+              {_.map(settingRoutesByName, ({ Component, label, ...rest }, key) => (
+                <Route
+                  key={key}
+                  path={key + '/*'}
+                  element={
+                    <Drawer label={label} path={'/' + key}>
+                      <Component />
+                    </Drawer>
+                  }
+                  {...rest}
+                />
+              ))}
+            </Routes>
+          </section>
+        </div>
       </DaisyUiThemeProvider>
     </Modal>
   )
