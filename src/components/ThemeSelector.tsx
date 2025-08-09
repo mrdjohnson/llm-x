e438e261929e6415e0fed7b3afa26d5905712d0a
djohnson <mr.demarcus.johnson@gmail.com>
2025-08-10 03:15:37 +0700

    migrate theme selector to mantine

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -1,8 +1,7 @@
+import { Select } from '@mantine/core'
 import _ from 'lodash'
-import { twMerge } from 'tailwind-merge'
 
 import { settingStore } from '~/core/setting/SettingStore'
-import { Select, SelectItem } from '@heroui/react'
 
 const themes = {
   _system: 'System theme',
@@ -19,35 +18,13 @@ const ThemeSelector = () => {
   return (
     <div className="form-control">
       <Select
-        className="w-full min-w-[20ch] rounded-md border border-base-content/30 bg-transparent"
-        size="sm"
-        classNames={{
-          value: '!text-base-content min-w-[20ch]',
-          trigger: 'bg-base-100 hover:!bg-base-200 rounded-md',
-          popoverContent: 'text-base-content bg-base-100',
-        }}
-        selectedKeys={[selectedTheme]}
+        size="md"
+        value={selectedTheme}
+        data={_.map(themes, (label, value) => ({ value, label }))}
         label="Theme"
-        onChange={selection =>
-          settingStore.update({ theme: selection.target.value as keyof typeof themes })
-        }
-      >
-        {_.map(themes, (value, key) => (
-          <SelectItem
-            key={key}
-            value={key}
-            className={twMerge(
-              'w-full !min-w-[13ch] text-base-content',
-              key === selectedTheme && 'text-primary',
-            )}
-            classNames={{
-              description: ' text',
-            }}
-          >
-            {value}
-          </SelectItem>
-        ))}
-      </Select>
+        onChange={theme => settingStore.update({ theme: theme as keyof typeof themes })}
+        allowDeselect={false}
+      />
     </div>
   )
 }
