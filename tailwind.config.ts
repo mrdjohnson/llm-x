203e89ca402b5ee6b2891a1d14412325ace3364b
djohnson <mr.demarcus.johnson@gmail.com>
2025-08-12 13:51:03 +0700

    squishy remove heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import { heroui } from '@heroui/react'
 import themes from 'daisyui/src/theming/themes'
 
 const errorColor = 'oklch(51% 0.17 22.1)'
@@ -14,20 +13,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui'),
-    heroui({
-      themes: {
-        light: {
-          colors: {
-            danger: errorColor,
-          },
-        },
-        dark: {
-          colors: {
-            danger: errorColor,
-          },
-        },
-      },
-    }),
   ],
   daisyui: {
     themes: [
